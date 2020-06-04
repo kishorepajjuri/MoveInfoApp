66ec0e36045ac26a78e1a9bb77632f4d53f64bc7
kishore <kishorjava91@gmail.com>
2020-06-04 12:46:45 +0530

    changes for tomcat
    Signed-off-by: kishore <kishorjava91@gmail.com>

--- src/main/java/com/micro/MovieInfoApp/MovieInfoAppApplication.java
+++ src/main/java/com/micro/MovieInfoApp/MovieInfoAppApplication.java
@@ -2,12 +2,19 @@ package com.micro.MovieInfoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MovieInfoAppApplication {
-
+public class MovieInfoAppApplication extends SpringBootServletInitializer {
+ 
+    @Override
+    protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder builder) {
+        return builder.sources(MovieInfoAppApplication.class);
+    }
 	
 	
 	
